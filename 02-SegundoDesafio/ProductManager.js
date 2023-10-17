5ccbeaceac1109e03745e5e0c9a62f5136ce1f19
Francisco Gelabert <50490304+franciscogelabert@users.noreply.github.com>
2023-10-17 10:53:04 -0300

    Update ProductManager.js
    Se prepara Archivo para entrega

--- 02-SegundoDesafio/ProductManager.js
+++ 02-SegundoDesafio/ProductManager.js
@@ -1,3 +1,24 @@
+class Product {
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+
+    esValido() {
+        return (
+            this.title &&
+            this.description &&
+            this.price &&
+            this.thumbnail &&
+            this.code &&
+            this.stock
+        );
+    }
+}
 
 
 class FileManager {
@@ -18,7 +39,7 @@ class FileManager {
         }
     }
 
-    getProducts() {
+    getItemsArchivo() {
         try {
             const data = this.fs.readFileSync(this.archivo, 'utf8');
             this.lista = JSON.parse(data);
@@ -47,29 +68,6 @@ class FileManager {
     }
 }
 
-class Product {
-    constructor(title, description, price, thumbnail, code, stock) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-    }
-
-    esValido() {
-        return (
-            this.title &&
-            this.description &&
-            this.price &&
-            this.thumbnail &&
-            this.code &&
-            this.stock
-        );
-    }
-}
-
-
 class ProductManager {
     constructor(fs) {
         this.id = 0;
@@ -78,6 +76,7 @@ class ProductManager {
     }
 
     addProduct = function (producto) {
+        this.fs.getItemsArchivo(); // actualizo lista con archivo
         if (!this.seEncuentra(producto.code) && producto.esValido()) {
             this.id = this.id + 1;
             this.lista.push(producto);
@@ -86,6 +85,7 @@ class ProductManager {
     };
 
     updateProduct = function (producto) {
+        this.fs.getItemsArchivo(); // actualizo lista con archivo
         for (let i = 0; i < this.lista.length; i++) {
             if (this.lista[i].code === producto.code) {
                 this.lista[i] = producto;
@@ -98,6 +98,8 @@ class ProductManager {
 
     // Buscar y eliminar un producto de la lista por su código
     eliminarProductoPorCodigo(code) {
+        
+        this.fs.getItemsArchivo(); // actualizo lista con archivo
         const productoIndex = this.lista.findIndex(producto => producto.code === code);
 
         if (productoIndex !== -1) {
@@ -110,6 +112,7 @@ class ProductManager {
     }
 
     getProductById = function (id) {
+        this.fs.getItemsArchivo(); // actualizo lista con archivo
         return this.lista[id] || `Not Found`
     };
 
@@ -122,55 +125,8 @@ class ProductManager {
     }
 
     getProductByCode = function (code) {
-        this.fs.getProducts();
+        this.fs.getItemsArchivo(); // actualizo lista con archivo
         return this.lista.find((element) => element.code == code) || `Not Found`;
     };
-
-
 }
 
-// Creo 3 productos 
-const p1 = new Product('Manzana', 'Fruta Manzana', 20, 'url imagen', 'cod1', 10);
-const p2 = new Product('Banana', 'Fruta Banana', 20, 'url imagen', 'cod2', 12);
-const p3 = new Product('Naranja', 'Fruta Naranja', 20, 'url imagen', 'cod3', 13);
-
-console.log('Paso 1 - Se crean los 3 productos');
-
-
-
-// crea Instancia del Product Manager y setea el nombre del Archivo, el Origen de fatos y la ruta
-const farchivo = new FileManager('archivo.json', 'C:/Coderhouse/Backend/02-SegundoDesafio');
-console.log('00- el archivo es',farchivo.archivo);
-
-// creo el ProductManager
-const lp = new ProductManager(farchivo);
-console.log('Paso 2 - Se crea el Product Manager');
-
-// le agrego los productos al ProductManager
-
-lp.addProduct(p1);
-lp.addProduct(p2);
-lp.addProduct(p3);
-
-
-console.log('Paso 3 - Se cargan los 3 productos en el Product Manager');
-
-
-const p4 = new Product('Berenjena', 'Verdura Berenjena', 20, 'url imagen', 'cod4', 14);
-lp.addProduct(p4);
-
-
-console.log('Paso 4 - Se carga el 4to producto');
-
-
-
-const p5 = new Product('Berenjena', 'Verdura', 20, 'url imagen', 'cod4', 80);
-lp.updateProduct(p5);
-console.log('se Actualiza el 4to Producto: ');
-
-
-
-lp.eliminarProductoPorCodigo('cod3');
-console.log('Se elimina el producto cod3: ', lp.lista);
-
-console.log(lp.getProductByCode('cod2'));
