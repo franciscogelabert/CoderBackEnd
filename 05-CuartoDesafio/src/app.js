097a12b7b035fd57b7d1447380f4633844a826bb
Francisco Gelabert <francisco.gelabert@gmail.com>
2023-11-06 12:44:50 -0300

    Agregado de admin

--- 05-CuartoDesafio/src/app.js
+++ 05-CuartoDesafio/src/app.js
@@ -1,19 +1,27 @@
-
-import { productsRouter } from './routes/products.router.js';
 import { viewsRouter } from './routes/views.router.js';
-import { viewsrealTimeProducts } from './routes/views.realTimeProducts.js';
 import express from 'express';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
 import { Server } from 'socket.io';
 
+import Product from '../class/Product/Product.js';
+import ProductManager from '../class/Product/ProductManager.js';
+import FileManager from '../class/FileSystem/FileManager.js';
+
+
+const farchivo = new FileManager('productos.json', 'C:/Proyectos/Coder/05-CuartoDesafio/files');
+//const farchivo = new FileManager('productos.json', 'C:/Coderhouse/Backend/05-CuartoDesafio/files');
+
+
+// creo el ProductManager
+const lp = new ProductManager(farchivo);
+console.log('Paso 1 - Se crea el Product Manager');
+
+// crea Instancia del Product Manager y setea el nombre del Archivo, el Origen de datos y la ruta
+
+
 const app = express();
 const port = 8080;
-const httpServer = app.listen(port, () => { console.log("Escuchando en Puerto: ", { port }) });
-
-export const socketServer = new Server(httpServer);
-
-
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`);
@@ -24,15 +32,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 // Conectar los routers a las rutas principales
-app.use('/api/products', productsRouter);
-app.use('/users', viewsRouter);
-app.use('/realtimeproducts', viewsrealTimeProducts);
+app.use('/', viewsRouter);
+
+
+const httpServer = app.listen(port, () => { console.log("Escuchando en Puerto: ", { port }) });
+const socketServer = new Server(httpServer);
+
+
+///////////////////////Añadir unelemento///////////////
 
 socketServer.on('connection', socket => {
     console.log('Nuevo Cliente Conectado (Server 1)')
-    socket.on('message', data => { console.log(data) });
-    socket.emit('individual', 'Individual');
-    socket.broadcast.emit('individualMenosYo', 'Individual Menos Yo');
-    socketServer.emit('todos','Todos')
+
+    socket.on('agregar_producto', (data) => {
+       
+        console.log("entra");
+        const thumbnail = [];
+        thumbnail.push(data.thumbnail1);
+        thumbnail.push(data.thumbnail2);
+        const newProduct = new Product(data.title, data.description, data.code, data.price, data.stock, thumbnail, data.estado, data.category);
+        lp.addProduct(newProduct);
+        socketServer.emit("productAdded", newProduct)
+    });
 });
 
+
+
+export default socketServer;
--- 05-CuartoDesafio/src/public/js/index.js
+++ 05-CuartoDesafio/src/public/js/index.js
@@ -1,19 +1,46 @@
+// crea sicket
 const socket = io();
+console.log("connected");
 
-socket.emit('message', 'hola desde el Cliente');
+// agrega listener para escuchar el click del botnon registar
 
-socket.on('individual', data => { console.log('individual -->', data) });
+let btn_registrar = document.getElementById('btn_registrar');
+console.log(btn_registrar);
+btn_registrar.addEventListener('click', (e) => {
+    e.preventDefault();
+ 
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    let code = document.getElementById('code').value
+    let price = document.getElementById('price').value
+    let stock = document.getElementById('stock').value
+    let thumbnaill = document.getElementById('thumbnail1').value
+    let thumbnail2 = document.getElementById('thumbnail2').value
+    let estado = document.getElementById('estado').value
+    let category = document.getElementById('category').value
 
-socket.on('individualMenosYo', data => { console.log('individualMenosYo -->', data) });
+    let new_product = {
+        "title": title,
+        "description": description,
+        "code": code,
+        "price": price,
+        "stock": stock,
+        "thumbnail": thumbnaill,
+        "thumbnai2": thumbnail2,
+        "estado": estado,
+        "category": category
+    };
+    socket.emit('agregar_producto', new_product);
+  
+})
 
-socket.on('todos', data => { console.log('todos--', data) });
-
-socket.on('addProduct', (product) => {
-    const productList = document.getElementById('pList');
-    const item = document.createElement('li');
-    item.setAttribute('id', product.code);
-    item.textContent = `${product.title}  ${product.description}  ${product.price}  ${product.stock}  ${product.category}`;
-    productList.appendChild(item);
+socket.on("productAdded",(product)=>{
+    const acualList = document.getElementById('pList');
+    const addElement = document.createElement('li');
+    addElement.setAttribute('id',product.code);
+    addElement.textContent=`${product.title} ${product.description} ${product.price} ${product.stock} ${product.category}`;
+    acualList.appendChild(addElement);
 });
+
 
 
